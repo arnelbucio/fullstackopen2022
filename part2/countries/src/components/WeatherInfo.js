7a5487579c26fc09b9ea95d205458451c3b92bf4
Arnel Bucio <rnlbucio@gmail.com>
2022-03-25 13:59:54 +0800

    Check if capitalInfo.latlng is an empty object will then just use country.latlng instead

--- part2/countries/src/components/WeatherInfo.js
+++ part2/countries/src/components/WeatherInfo.js
@@ -4,12 +4,11 @@ import axios from 'axios'
 const WeatherInfo = ({country}) => {
   const [weather, setWeather] = useState(null)
   const api_key = process.env.REACT_APP_API_KEY
-  const latlng = country.latlng || country.capitalInfo.latlng
-
+  const latlng = (Object.keys(country.capitalInfo ).length === 0) ? country.latlng : country.capitalInfo.latlng
 
   useEffect(() => {
     axios
-      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latlng[0]}&lon=${latlng[0]}&appid=${api_key}&units=metric`)
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latlng[0]}&lon=${latlng[1]}&appid=${api_key}&units=metric`)
       .then(response => {
         setWeather(response.data)
       }).catch(error => {
