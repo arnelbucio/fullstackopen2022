dc998ea46d5c220d298acfe79b552aa0e9f40aad
Arnel Bucio <rnlbucio@gmail.com>
2022-03-25 13:35:43 +0800

    Fix countries app bugs

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,22 +1,8 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState } from 'react'
+import WeatherInfo from './WeatherInfo'
 
 const Country = ({country, forceShowData}) => {
   const [isShowData, showData] = useState(false)
-  const [weather, setWeather] = useState([])
-  const latlng = country.capitalInfo.latlng
-  const api_key = process.env.REACT_APP_API_KEY
-
-  useEffect(() => {
-      axios
-        .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latlng[0]}&lon=${latlng[1]}&appid=${api_key}&units=metric`)
-        .then(response => {
-          setWeather(response.data)
-        }).catch(error => {
-          console.log(error)
-        })
-  }, [])
-
 
   if (isShowData || forceShowData) {
     return (
@@ -34,15 +20,7 @@ const Country = ({country, forceShowData}) => {
           </ul>
         </div>
         <img src={country.flags.png} alt={country.name.common} />
-        {(Object.keys(weather).length !== 0)
-          ? (<div>
-              <h2>Weather in {country.capital}</h2>
-              <p>temperature {weather.main.temp} Celcius</p>
-              <p><img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} /></p>
-              <p>wind {weather.wind.speed} m/s</p>
-            </div>)
-          : ''
-        }
+        <WeatherInfo country={country} />
       </div>
     )
   }
---
+++ part2/countries/src/components/WeatherInfo.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const WeatherInfo = ({country}) => {
+  const [weather, setWeather] = useState(null)
+  const api_key = process.env.REACT_APP_API_KEY
+  const latlng = country.latlng || country.capitalInfo.latlng
+
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latlng[0]}&lon=${latlng[0]}&appid=${api_key}&units=metric`)
+      .then(response => {
+        setWeather(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+}, [])
+
+
+  return weather && (
+    <div>
+      <h2>Weather in {country.capital}</h2>
+      <p>temperature {weather.main.temp} Celcius</p>
+      <p><img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} /></p>
+      <p>wind {weather.wind.speed} m/s</p>
+    </div>
+  )
+}
+
+
+export default WeatherInfo
