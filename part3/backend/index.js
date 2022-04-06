0e4d99956a7245f89bf594c2c1da24553888bdd1
Arnel Bucio <rnlbucio@gmail.com>
2022-04-06 12:17:47 +0800

    Fix .end() on delete request

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -47,7 +47,7 @@ app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
 
-  response.status(204).end
+  response.status(204).end()
 })
 
 
