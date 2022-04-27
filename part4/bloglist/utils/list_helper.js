04a7f9c1e81827ed2d5f4383713eea6331a165e3
Arnel Bucio <rnlbucio@gmail.com>
2022-04-27 16:06:04 +0800

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,6 +10,22 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const favorite = blogs.reduce((previous, current) => {
+    return (previous.likes > current.likes)
+      ? previous
+      : current
+  }, {})
+
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
