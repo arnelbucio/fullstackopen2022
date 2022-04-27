b52bd0f8d206cee6328bd966cfa485be16ba6740
Arnel Bucio <rnlbucio@gmail.com>
2022-04-27 13:07:49 +0800

    4.1 Blog list, step1

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -13,7 +14,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
---
+++ part4/bloglist/readme.md
@@ -0,0 +1,9 @@
+#Bloglist
+
+`npm install` to install
+
+
+edit .env.sample with your mongodb atlas username and password then rename .env.sample to .env
+
+
+`npm run dev` to start
