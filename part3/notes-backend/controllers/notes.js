3437b408725c01256d4653a4cf8b02d08d430591
Arnel Bucio <rnlbucio@gmail.com>
2022-05-12 14:54:51 +0800

    Part 4b Error handling and async/await

--- part3/notes-backend/controllers/notes.js
+++ part3/notes-backend/controllers/notes.js
@@ -6,16 +6,17 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (request, response, next) => {
+  try {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 notesRouter.post('/', async (request, response, next) => {
@@ -35,12 +36,13 @@ notesRouter.post('/', async (request, response, next) => {
   }
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 notesRouter.put('/:id', (request, response, next) => {
--- part3/notes-backend/tests/note_api.test.js
+++ part3/notes-backend/tests/note_api.test.js
@@ -74,6 +74,35 @@ test('note without content is not added', async () => {
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
 })
 
+test('a specific note can be viewd', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+
+  expect(resultNote.body).toEqual(processedNoteToView)
+})
+
+test('a note can be delete', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToDelete = notesAtStart[0]
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1)
+
+  const contents = notesAtEnd.map(r => r.content)
+  expect(contents).not.toContain(noteToDelete.content)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
