f2acaaf32c361c93e8869aeb58db86fde04a9ea4
Arnel Bucio <rnlbucio@gmail.com>
2022-05-19 00:06:09 +0800

    Fix notes app

--- part3/part2-notes/src/index.js
+++ part3/part2-notes/src/index.js
@@ -1,5 +1,8 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(document.getElementById('root')).render(<App />)
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
