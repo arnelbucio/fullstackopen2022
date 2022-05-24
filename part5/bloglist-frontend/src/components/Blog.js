f987aed4513a63ff926e0d657fefff36115d8087
Arnel Bucio <rnlbucio@gmail.com>
2022-05-24 14:52:54 +0800

    Finish exercise 5.7* Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,38 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+  const [toggleButtonText, setToggleButtonText] = useState('view')
+
+  const detailsStyle = { display: detailsVisible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+  const toggleDetails = () => {
+    setDetailsVisible(!detailsVisible)
+    setToggleButtonText(toggleButtonText === 'view' ? 'hide' : 'view')
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleDetails}>{toggleButtonText}</button>
+      </div>
+      <div style={detailsStyle}>
+        {blog.url}<br />
+        {blog.likes} <button>like</button><br />
+        {blog.author}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
