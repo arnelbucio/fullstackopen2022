a72ccd5565c9cb854a684f947292e4ccfdfcc218
Arnel Bucio <rnlbucio@gmail.com>
2022-05-26 17:22:05 +0800

    Part 5c Testing React apps

--- part3/part2-notes/src/components/Note.js
+++ part3/part2-notes/src/components/Note.js
@@ -3,8 +3,8 @@ const Note = ({ note, toggleImportance }) => {
     ? 'make not important' : 'make important'
 
   return (
-    <li className="note">
-      {note.content} 
+    <li className='note'>
+      {note.content}
       <button onClick={toggleImportance}>{label}</button>
     </li>
   )
---
+++ part3/part2-notes/src/components/Note.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import Note from './Note'
+
+test('renders content', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const { container } = render(<Note note={note} />)
+
+  const div = container.querySelector('.note')
+  expect(div).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  )
+})
