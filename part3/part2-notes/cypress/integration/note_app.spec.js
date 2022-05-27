bc2defab85e6851865fd770f9ae41213ac7ca392
Arnel Bucio <rnlbucio@gmail.com>
2022-05-27 18:02:46 +0800

    Part 5d End to end testing - Cypress

---
+++ part3/part2-notes/cypress/integration/note_app.spec.js
@@ -0,0 +1,14 @@
+describe('Note app', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('login').click()
+  })
+})
