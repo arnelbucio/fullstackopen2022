df4184a85c23a9ffdd8881581e86416aadc54a1a
Arnel Bucio <rnlbucio@gmail.com>
2022-05-27 18:11:54 +0800

    Part 5d Testing new note form

--- part3/part2-notes/cypress/integration/note_app.spec.js
+++ part3/part2-notes/cypress/integration/note_app.spec.js
@@ -20,4 +20,20 @@ describe('Note app', function() {
 
     cy.contains('Superuser logged in')
   })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('login').click()
+      cy.get('#username').type('root')
+      cy.get('#password').type('root')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+  })
 })
