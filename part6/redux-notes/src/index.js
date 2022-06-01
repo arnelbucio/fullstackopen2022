eed2c64c048f4e9edc2df2013f36b7706efa3f11
Arnel Bucio <rnlbucio@gmail.com>
2022-06-01 14:55:58 +0800

    Part 6d Uncontrolled form

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -25,12 +25,45 @@ store.dispatch({
   }
 })
 
+const generateId = () => {
+  Number((Math.random() * 1000000).toFixed(0))
+}
+
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+
+    store.dispatch({
+      type: 'NEW_NOTE',
+      data: {
+        content,
+        important: false,
+        id: generateId()
+      }
+    })
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      data: { id }
+    })
+  }
+
   return(
     <div>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
       <ul>
         {store.getState().map(note=>
-          <li key={note.id}>
+          <li
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}
