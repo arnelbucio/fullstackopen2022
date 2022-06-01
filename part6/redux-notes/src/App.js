3f04258cc78967e1877708ab9bd1be3c45ea6d26
Arnel Bucio <rnlbucio@gmail.com>
2022-06-01 15:49:49 +0800

    Part 6a fix

--- part6/redux-notes/src/App.js
+++ part6/redux-notes/src/App.js
@@ -1,5 +1,3 @@
-import { createNote, toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 
