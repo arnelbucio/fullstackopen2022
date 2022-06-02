c0396c631120344585448cd8cdbd34ce852e4c76
Arnel Bucio <rnlbucio@gmail.com>
2022-06-02 13:01:58 +0800

    Part 6b Many reducers

--- part6/redux-notes/src/App.js
+++ part6/redux-notes/src/App.js
@@ -1,14 +1,15 @@
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
+import VisibilityFilter from './components/Visibilityfilter'
 
 const App = () => {
   return(
     <div>
       <NewNote />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
 }
-
 
 export default App
--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+const store = createStore(reducer)
 
-const store = createStore(noteReducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
