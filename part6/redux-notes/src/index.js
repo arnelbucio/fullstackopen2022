437f4c8f9484ba5480207f1e124eba5cb9f8a9f6
Arnel Bucio <rnlbucio@gmail.com>
2022-06-03 18:38:28 +0800

    Part 6c Asynchronous actions and redux thunk

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -1,24 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
+import store from './store'
 
-import noteReducer, { setNotes } from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-import noteService from './services/notes'
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-
-noteService.getAll().then(notes =>
-  store.dispatch(setNotes(notes))
-)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
