6d5a79cc4908a68a4d52ed67ea3b453d6df309f7
Arnel Bucio <rnlbucio@gmail.com>
2022-06-03 17:01:12 +0800

    Part 6c Communicating with server in a redux application

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -4,8 +4,10 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 
-import noteReducer from './reducers/noteReducer'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
+
+import noteService from './services/notes'
 
 const store = configureStore({
   reducer: {
@@ -13,6 +15,10 @@ const store = configureStore({
     filter: filterReducer
   }
 })
+
+noteService.getAll().then(notes =>
+  store.dispatch(setNotes(notes))
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
