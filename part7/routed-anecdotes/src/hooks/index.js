201a64d7918fc90aad35d8521af9ef674ace9dda
Arnel Bucio <rnlbucio@gmail.com>
2022-06-06 13:33:34 +0800

    Finish exercise 7.4: anecdotes and hooks step1

---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+export const useField = (name, type = 'text') => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+
+  return {
+    name,
+    type,
+    value,
+    onChange
+  }
+}
