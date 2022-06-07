2122958df4d995948c0b835e3f3feb85c5eef1fa
Arnel Bucio <rnlbucio@gmail.com>
2022-06-07 12:04:21 +0800

    Part 7d Webpack

---
+++ part7/webpack-part7/src/index.js
@@ -0,0 +1,10 @@
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import App from './App'
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <App />
+)
