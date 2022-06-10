752339f9b86d0dc23b918c100af3afc699ae4383
Arnel Bucio <rnlbucio@gmail.com>
2022-06-10 20:27:10 +0800

    Finish exercise 8.8: Authors view

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -94,6 +94,7 @@ const typeDefs = gql`
     name: String!
     born: Int
     bookCount: Int!
+    id: ID!
   }
   type Book {
     title: String!
--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,8 +1,16 @@
-const Authors = (props) => {
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries'
+
+const Authors = props => {
+  const result = useQuery(ALL_AUTHORS)
   if (!props.show) {
     return null
   }
-  const authors = []
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  const authors = result.data.allAuthors
 
   return (
     <div>
@@ -14,7 +22,7 @@ const Authors = (props) => {
             <th>born</th>
             <th>books</th>
           </tr>
-          {authors.map((a) => (
+          {authors.map(a => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
