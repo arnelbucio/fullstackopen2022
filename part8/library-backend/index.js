95634322a2b44895ba505d1a0ca860845eb4f0b0
Arnel Bucio <rnlbucio@gmail.com>
2022-06-10 11:44:56 +0800

    Finish exercise 8.2: All books

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -89,9 +89,17 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+    id: ID!
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -99,6 +107,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books,
   },
 }
 
