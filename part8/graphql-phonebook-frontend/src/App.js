2e4d532948a4215b2eae93a01eac3028a350eea0
Arnel Bucio <rnlbucio@gmail.com>
2022-06-10 18:47:16 +0800

    Part 8b Making queries

--- part8/graphql-phonebook-frontend/src/App.js
+++ part8/graphql-phonebook-frontend/src/App.js
@@ -1,5 +1,25 @@
+import { gql, useQuery } from '@apollo/client'
+
+import Persons from './components/Persons'
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`
+
 const App = () => {
-  return <></>
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return <Persons persons={result.data.allPersons} />
 }
 
 export default App
