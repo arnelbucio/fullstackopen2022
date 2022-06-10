6f3fcc51b46827f3d7fab5d4e7176c2e0d0fcdee
Arnel Bucio <rnlbucio@gmail.com>
2022-06-10 19:39:49 +0800

    Part 8b Updating a phone number

--- part8/graphql-phonebook-frontend/src/App.js
+++ part8/graphql-phonebook-frontend/src/App.js
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { useQuery } from '@apollo/client'
 
+import { ALL_PERSONS } from './queries'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Notify from './components/Notify'
-import { ALL_PERSONS } from './queries'
+import PhoneForm from './components/PhoneForm'
 
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
@@ -27,6 +28,7 @@ const App = () => {
       <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </div>
   )
 }
