92c1130f5bcded6371501bd31e56c333dd317753
Arnel Bucio <rnlbucio@gmail.com>
2022-06-13 14:01:49 +0800

    Part 8d Logout

--- part8/graphql-phonebook-frontend/src/App.js
+++ part8/graphql-phonebook-frontend/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useQuery } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client'
 
 import { ALL_PERSONS } from './queries'
 import Persons from './components/Persons'
@@ -11,8 +11,8 @@ import LoginForm from './components/LoginForm'
 const App = () => {
   const [errorMessage, setErrorMessage] = useState(null)
   const [token, setToken] = useState(null)
-
   const result = useQuery(ALL_PERSONS)
+  const client = useApolloClient()
 
   const notify = message => {
     setErrorMessage(message)
@@ -21,13 +21,18 @@ const App = () => {
     }, 10000)
   }
 
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
   if (!token) {
     return (
-      <div>
+      <>
         <Notify errorMessage={errorMessage} />
-        <h2>Login</h2>
         <LoginForm setToken={setToken} setError={notify} />
-      </div>
+      </>
     )
   }
 
@@ -38,6 +43,7 @@ const App = () => {
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
