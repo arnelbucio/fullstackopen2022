47a4ed2c4e435df52b270a44946a26d5d0f45618
Arnel Bucio <rnlbucio@gmail.com>
2022-06-20 11:51:30 +0800

    Part 9b First steps with TypeScript

---
+++ part9/typescript-app/multiplier.ts
@@ -0,0 +1,5 @@
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a * b)
+}
+
+multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
