c29a7dae72cf3400be2881285418063270102e10
Arnel Bucio <rnlbucio@gmail.com>
2022-06-21 11:47:39 +0800

    Part 9b The horrors of any

--- part9/typescript-app/index.ts
+++ part9/typescript-app/index.ts
@@ -1,12 +1,28 @@
+import { calculator } from './calculator';
 import express from 'express';
 const app = express();
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
-})
+});
+
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if ( !value1 || isNaN(Number(value1))) {
+    return res.send({ error: '...'}).status(400);
+  }
+
+  // more validations here...
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const result = calculator(value1, value2, op);
+  return res.send(result);
+});
 
 const PORT = 3003;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
