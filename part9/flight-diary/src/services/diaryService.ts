cf8ace5a1e2e6d14dbc32cf60d54d582ccd8bf89
Arnel Bucio <rnlbucio@gmail.com>
2022-06-22 12:25:20 +0800

    Part 9c Adding a new diary

--- part9/flight-diary/src/services/diaryService.ts
+++ part9/flight-diary/src/services/diaryService.ts
@@ -1,5 +1,9 @@
 import diaries from '../../data/diaries';
-import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import {
+  DiaryEntry,
+  NonSensitiveDiaryEntry,
+  NewDiaryEntry
+} from '../types';
 
 const getEntries = (): Array<DiaryEntry> => {
   return diaries;
@@ -14,8 +18,14 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addDiary = () => {
-  return [];
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 const findById = (id: number): DiaryEntry | undefined => {
--- part9/flight-diary/src/types.ts
+++ part9/flight-diary/src/types.ts
@@ -1,6 +1,5 @@
 export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy';
 export type Visibility = 'great' | 'good' | 'ok' | 'poor';
-export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
 
 export interface DiaryEntry {
   id: number;
@@ -9,3 +8,6 @@ export interface DiaryEntry {
   visibility: Visibility;
   comment: string;
 }
+
+export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>;
