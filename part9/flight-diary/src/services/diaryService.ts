c931f66c11f7f94c90d46b89b77ac9590172c220
Arnel Bucio <rnlbucio@gmail.com>
2022-06-22 11:28:09 +0800

    Part 9c Utility types

--- part9/flight-diary/src/services/diaryService.ts
+++ part9/flight-diary/src/services/diaryService.ts
@@ -1,15 +1,26 @@
 import diaries from '../../data/diaries';
-import { DiaryEntry } from '../types';
+import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
 
 const getEntries = (): Array<DiaryEntry> => {
   return diaries;
 };
 
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility}) => ({
+    id,
+    date,
+    weather,
+    visibility
+  }));
+};
+
+
 const addDiary = () => {
-  return null;
+  return [];
 };
 
 export default {
   getEntries,
-  addDiary
+  addDiary,
+  getNonSensitiveEntries
 };
