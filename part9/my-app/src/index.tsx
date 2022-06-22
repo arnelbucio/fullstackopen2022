673f80e1f94679640d92a5237f3513d8cd5134ea
Arnel Bucio <rnlbucio@gmail.com>
2022-06-22 17:03:20 +0800

    Part 9d React components with TypeScript

--- part9/my-app/src/index.tsx
+++ part9/my-app/src/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+interface WelcomeProps {
+  name: string;
+}
+
+const Welcome = (props: WelcomeProps) => {
+  return <h1>Hello, {props.name}</h1>;
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Welcome name="Sara" />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
